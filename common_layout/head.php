e89e4ca970ec0e490e73a111ed2b12811753da68
Rafael Pedroso <rafaelcalvaria@gmail.com>
2019-05-19 20:48:13 -0300

    Add Google Analytic - Código de acompanhamento

--- common_layout/head.php
+++ common_layout/head.php
@@ -1,5 +1,15 @@
-<!-- Meta tags Obrigatórias -->
-<meta charset="utf-8">
+    <!-- Google Analytics Acompanhamento -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-140491285-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-140491285-1');
+    </script>
+    
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
